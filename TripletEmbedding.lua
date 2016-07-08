742599ce761b2d6cf128068b8f6fd327ad633a31
Bartosz Ludwiczuk <melgor89@gmail.com>
2016-07-08 18:16:24 +0200

    MRG - Merge pull request #3 from melgor/master

--- TripletEmbedding.lua
+++ TripletEmbedding.lua
@@ -18,11 +18,7 @@ function TripletEmbeddingCriterion:updateOutput(input)
    local p = input[2] -- positive
    local n = input[3] -- negative
    local N = a:size(1)
-   self.Li:resize(N)
-   for i = 1, N do
-      self.Li[i] = math.max(0, (a[i]-p[i])*(a[i]-p[i])+self.alpha-(a[i]-n[i])*(a[i]-n[i]))
-      --print(self.Li[i])
-   end
+   self.Li = torch.max(torch.cat(torch.Tensor(N):zero():type(torch.type(a)) , (a - p):norm(2,2):pow(2) -  (a - n):norm(2,2):pow(2) + self.alpha, 2), 2)
    self.output = self.Li:sum() / N
    return self.output
 end
@@ -32,14 +28,10 @@ function TripletEmbeddingCriterion:updateGradInput(input)
    local p = input[2] -- positive
    local n = input[3] -- negative
    local N = a:size(1)
-   if torch.type(a) == 'torch.CudaTensor' then -- if buggy CUDA API
-      self.gradInput[1] = (n - p):cmul(self.Li:gt(0):repeatTensor(a:size(2),1):t():type(a:type()) * 2/N)
-      self.gradInput[2] = (p - a):cmul(self.Li:gt(0):repeatTensor(a:size(2),1):t():type(a:type()) * 2/N)
-      self.gradInput[3] = (a - n):cmul(self.Li:gt(0):repeatTensor(a:size(2),1):t():type(a:type()) * 2/N)
-   else -- otherwise
-      self.gradInput[1] = self.Li:gt(0):diag():type(a:type()) * (n - p) * 2/N
-      self.gradInput[2] = self.Li:gt(0):diag():type(a:type()) * (p - a) * 2/N
-      self.gradInput[3] = self.Li:gt(0):diag():type(a:type()) * (a - n) * 2/N
-   end
+   
+   self.gradInput[1] = (n - p):cmul(self.Li:gt(0):repeatTensor(a:size(2),1):t():type(a:type()) * 2/N)
+   self.gradInput[2] = (p - a):cmul(self.Li:gt(0):repeatTensor(a:size(2),1):t():type(a:type()) * 2/N)
+   self.gradInput[3] = (a - n):cmul(self.Li:gt(0):repeatTensor(a:size(2),1):t():type(a:type()) * 2/N)
+
    return self.gradInput
 end
